d4026574fbe52653c84fb5bfbcea69e5a6ff550d
frankNowinski <nowinski.frank@gmail.com>
2015-10-14 22:29:38 -0400

    Update berserk_player.rb

--- lib/studio_game/berserk_player.rb
+++ lib/studio_game/berserk_player.rb
@@ -1,24 +1,24 @@
 require_relative 'player'
 
 module StudioGame
-	class BerserkPlayer < Player
-		def initialize(name, health=100)
-			super(name, health)
-			@w00t_count = 0
-		end
+  class BerserkPlayer < Player
+    def initialize(name, health=100)
+      super(name, health)
+      @w00t_count = 0
+    end
+    
+    def berserk?
+      @w00t_count > 5
+    end
 
-		def blam
-			berserk? ? w00t : super
-		end
+    def blam
+      berserk? ? w00t : super
+    end
 
-		def w00t
-			super
-			@w00t_count += 1
-			puts "#{@name} is going BIZZZERK!" if berserk?
-		end
-
-		def berserk?
-			@w00t_count > 5
-		end
-	end
+    def w00t
+      super
+      @w00t_count += 1
+      puts "#{@name} is going BIZZZERK!" if berserk?
+    end
+  end
 end
