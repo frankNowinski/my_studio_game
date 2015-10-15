f3c290bdeb38e1ecf6a1f03ccb66cc2cfa1e68a7
frankNowinski <nowinski.frank@gmail.com>
2015-10-14 22:50:07 -0400

    Update player.rb

--- lib/studio_game/player.rb
+++ lib/studio_game/player.rb
@@ -4,49 +4,49 @@ require_relative 'treasure_trove'
 require_relative 'playable'
 
 module StudioGame
-	class Player
-		include Playable
+  class Player
+    include Playable
 
-		attr_reader :health
-		attr_accessor :name
+    attr_reader :health
+    attr_accessor :name
 		
-		def initialize(name, health=100)
-			@name = name.capitalize
-			@health = health
-			@found_treasures = Hash.new(0)
-		end
+    def initialize(name, health=100)
+      @name = name.capitalize
+      @health = health
+      @found_treasures = Hash.new(0)
+    end
 
-		def name=(new_name)
-			@name = new_name.capitalize
-		end
+    def name=(new_name)
+      @name = new_name.capitalize
+    end
 
-		def found_treasure(treasure)
-			@found_treasures[treasure.name] += treasure.points
-			puts "#{name} found a #{treasure.name} worth #{treasure.points} points." 
-			puts "#{name}'s' treasures: #{@found_treasures}"
-		end
+    def found_treasure(treasure)
+      @found_treasures[treasure.name] += treasure.points
+      puts "#{name} found a #{treasure.name} worth #{treasure.points} points." 
+      puts "#{name}'s' treasures: #{@found_treasures}"
+    end
 
-		def each_found_treasure
-			@found_treasures.each do |name, points|
-				yield Treasure.new(name, points)
-			end
-		end
+    def each_found_treasure
+      @found_treasures.each do |name, points|
+	yield Treasure.new(name, points)
+      end
+    end
 
-		def points
-			@found_treasures.values.reduce(0, :+)
-		end
+    def points
+      @found_treasures.values.reduce(0, :+)
+    end
 
-		def score
-			@health + points
-		end
+    def score
+      @health + points
+    end
 
-		def <=>(other)
-			other.score <=> score
-		end
+    def <=>(other)
+      other.score <=> score
+    end
 
-		def to_s
-			"#{@name} has a health of #{@health}, points value of #{points}, and score of #{score}."
-		end
-	end
+    def to_s
+      "#{@name} has a health of #{@health}, points value of #{points}, and score of #{score}."
+    end
+  end
 end
 
