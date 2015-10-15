dd2930e5b9eb7b07dd57bd220aac381b1c9c2a3a
frankNowinski <nowinski.frank@gmail.com>
2015-10-14 22:46:22 -0400

    Update playable.rb

--- lib/studio_game/playable.rb
+++ lib/studio_game/playable.rb
@@ -1,17 +1,17 @@
 module StudioGame
-	module Playable
-		attr_accessor :health
+  module Playable
+    attr_accessor :health
 
-		def blam
-			self.health -= 10
-		end
+    def blam
+      self.health -= 10
+    end
 
-		def w00t
-			self.health += 15
-		end
+    def w00t
+      self.health += 15
+    end
 
-		def strong?
-			health > 100 ? true : false
-		end
-	end
+    def strong?
+      health > 100 ? true : false
+    end
+  end
 end
