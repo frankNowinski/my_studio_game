64bdf9a455e4507cebf188057c9c83e431bbda69
frankNowinski <nowinski.frank@gmail.com>
2015-10-14 22:44:14 -0400

    Update game_turn.rb

--- lib/studio_game/game_turn.rb
+++ lib/studio_game/game_turn.rb
@@ -3,24 +3,24 @@ require_relative 'playable'
 require_relative 'loaded_die'
 
 module StudioGame	
-	module GameTurn
-		def self.take_turn(player)
-			die = Die.new
+  module GameTurn
+    def self.take_turn(player)
+      die = Die.new
 
-			case die.roll
-			when 5..6
-				player.w00t
-				puts "#{player.name} rolled a #{die.number} and got w00ted!"
-			when 1..2
-				player.blam
-				puts "#{player.name} rolled a #{die.number} and got blammed!"
-			else
-				puts "#{player.name} rolled a #{die.number} and got skipped"
-			end
-			treasure = TreasureTrove.random
-			player.found_treasure(treasure)
-		end
-	end
+      case die.roll
+      when 5..6
+	player.w00t
+	puts "#{player.name} rolled a #{die.number} and got w00ted!"
+      when 1..2
+	player.blam
+	puts "#{player.name} rolled a #{die.number} and got blammed!"
+      else
+	puts "#{player.name} rolled a #{die.number} and got skipped"
+      end
+      treasure = TreasureTrove.random
+      player.found_treasure(treasure)
+    end
+  end
 end
 
 
